2eb43a83d75aae9723dd0dd62eb863acd8e82561
deshwal31055 <mdeshwal388@gmail.com>
2025-06-28 00:45:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let current = 1;
-function nextScene() {
-  const currentScene = document.getElementById(`scene${current}`);
-  if (currentScene) currentScene.style.display = "none";
-  current++;
-  const nextScene = document.getElementById(`scene${current}`);
-  if (nextScene) nextScene.style.display = "block";
-}
